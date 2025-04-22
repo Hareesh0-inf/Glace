7a1b6205e896f95565ff46316fc42a40a58568da
hareesh0-Inf <hareeshking04@gmail.com>
2025-04-22 16:57:19 +0000

    added enter and backspace

--- glace.c
+++ glace.c
@@ -332,12 +332,12 @@ void editorProcessKeypress() {
   int c = editorReadKey();
   switch (c) {
     case '\r':
-    /* TODO */
+      editorInsertNewline();
     break;
     case CTRL_KEY('c'):
     if (E.dirty && quit_times > 0) {
       editorSetStatusMessage("WARNING!!! File has unsaved changes. "
-        "Press Ctrl-Q %d more times to quit.", quit_times);
+        "Press Ctrl-C %d more times to quit.", quit_times);
       quit_times--;
       return;
     }
@@ -424,9 +424,10 @@ void editorUpdateRow(erow *row) {
   row->rsize = idx;
 }
 
-void editorAppendRow(char *s, size_t len) {
+void editorInsertRow(int at, char *s, size_t len) {
+  if (at < 0 || at > E.numrows) return;
   E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
-  int at = E.numrows;
+  memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at)); 
   E.row[at].rsize = 0;
   E.row[at].render = NULL;
   E.row[at].size = len;
@@ -435,6 +436,19 @@ void editorAppendRow(char *s, size_t len) {
   E.row[at].chars[len] = '\0';
   editorUpdateRow(&E.row[at]);
   E.numrows++;
+  E.dirty++;
+}
+
+
+void editorFreeRow(erow *row) {
+  free(row->render);
+  free(row->chars);
+}
+void editorDelRow(int at) {
+  if (at < 0 || at >= E.numrows) return;
+  editorFreeRow(&E.row[at]);
+  memmove(&E.row[at], &E.row[at + 1], sizeof(erow) * (E.numrows - at - 1));
+  E.numrows--;
   E.dirty++;
 }
 
@@ -448,6 +462,30 @@ void editorRowInsertChar(erow *row, int at, int c) {
   E.dirty++;
 }
 
+void editorRowAppendString(erow *row, char *s, size_t len) {
+  row->chars = realloc(row->chars, row->size + len + 1);
+  memcpy(&row->chars[row->size], s, len);
+  row->size += len;
+  row->chars[row->size] = '\0';
+  editorUpdateRow(row);
+  E.dirty++;
+}
+
+void editorInsertNewline() {
+  if (E.cx == 0) {
+    editorInsertRow(E.cy, "", 0);
+  } else {
+    erow *row = &E.row[E.cy];
+    editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+    row = &E.row[E.cy];
+    row->size = E.cx;
+    row->chars[row->size] = '\0';
+    editorUpdateRow(row);
+  }
+  E.cy++;
+  E.cx = 0;
+}
+
 void editorRowDelChar(erow *row, int at) {
   if (at < 0 || at >= row->size) return;
   memmove(&row->chars[at], &row->chars[at + 1], row->size - at);
@@ -459,7 +497,7 @@ void editorRowDelChar(erow *row, int at) {
 /*** editor operations ***/
 void editorInsertChar(int c) {
   if (E.cy == E.numrows) {
-    editorAppendRow("", 0);
+    editorInsertRow(E.numrows, "", 0);
   }
   editorRowInsertChar(&E.row[E.cy], E.cx, c);
   E.cx++;
@@ -467,18 +505,16 @@ void editorInsertChar(int c) {
 
 void editorDelChar() {
   if (E.cy == E.numrows) return;
+  if (E.cx == 0 && E.cy == 0) return;
   erow *row = &E.row[E.cy];
   if (E.cx > 0) {
     editorRowDelChar(row, E.cx - 1);
     E.cx--;
-  }
-}
-void editorDelChar() {
-  if (E.cy == E.numrows) return;
-  erow *row = &E.row[E.cy];
-  if (E.cx > 0) {
-    editorRowDelChar(row, E.cx - 1);
-    E.cx--;
+  } else {
+    E.cx = E.row[E.cy - 1].size;
+    editorRowAppendString(&E.row[E.cy - 1], row->chars, row->size);
+    editorDelRow(E.cy);
+    E.cy--;
   }
 }
 
@@ -532,7 +568,7 @@ void editorOpen(char* filename) {
     while (linelen > 0 && (line[linelen - 1] == '\n' ||
                            line[linelen - 1] == '\r'))
       linelen--; 
-    editorAppendRow(line, linelen);
+      editorInsertRow(E.numrows, line, linelen);
   }
   free(line);
   fclose(fp);
